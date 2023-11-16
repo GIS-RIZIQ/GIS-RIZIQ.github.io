261354c73db9a267dc700757662b4cb263b62043
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-11-17 00:33:58 +0700

    js

--- js/template/template.js
+++ js/template/template.js
@@ -3,10 +3,10 @@
 
 import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
 
-export let URLGeoJson = "https://us-central1-fahad-402509.cloudfunctions.net/gis-gcf";
-export let urlPostGCF = "https://us-central1-fahad-402509.cloudfunctions.net/postloc";
-export let UrlLogin = "https://us-central1-fahad-402509.cloudfunctions.net/hadlogin-gis"
-export let UrlRegister = "https://us-central1-fahad-402509.cloudfunctions.net/gisregis"
+export let URLGeoJson = "https://us-central1-gcpgeospacial.cloudfunctions.net/geojsonriziq";
+export let urlPostGCF = "https://us-central1-gcpgeospacial.cloudfunctions.net/gis4";
+export let UrlLogin = "https://us-central1-gcpgeospacial.cloudfunctions.net/gis"
+export let UrlRegister = "https://us-central1-gcpgeospacial.cloudfunctions.net/registergis"
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let token = ""
