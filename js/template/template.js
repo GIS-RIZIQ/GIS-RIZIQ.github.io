c815281ec6e8b80e4855f3b4ce4759906f83345d
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-11-17 13:20:17 +0700

    js

--- js/template/template.js
+++ js/template/template.js
@@ -1,10 +1,13 @@
+import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+
 export let URLGeoJson = "https://us-central1-gcpgeospacial.cloudfunctions.net/geojsonriziq";
 export let urlPostGCF = "https://us-central1-gcpgeospacial.cloudfunctions.net/gis4";
+export let UrlLogin = "https://us-central1-gcpgeospacial.cloudfunctions.net/gis"
+export let UrlRegister = "https://us-central1-gcpgeospacial.cloudfunctions.net/registergis"
 export let tableTag="tr";
 export let tableRowClass="content is-small";
-export let URLPost = `https://us-central1-gcpgeospacial.cloudfunctions.net/registergis`
-export let URLLogin = `https://us-central1-gcpgeospacial.cloudfunctions.net/gis`
-export let token = 'token';
+export let token = "token"
+export let cookie = getCookie("Login")
 export let tableTemplate=`
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
